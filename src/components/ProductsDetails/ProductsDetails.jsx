e27842fbfd1480fab85a7d235a607e05255f931e
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-24 18:08:09 +0600

    Update script and stylesheets

--- src/components/ProductsDetails/ProductsDetails.jsx
+++ src/components/ProductsDetails/ProductsDetails.jsx
@@ -6,7 +6,7 @@ const ProductsDetails = () => {
     // @ts-ignore
     const { title, description, image } = data;
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <div className="flex flex-col items-center justify-center w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
             <h1 className="text-4xl font-bold text-gray-800 mb-4">{ title }</h1>
             <p className="text-lg text-gray-600 mb-8">{ description }</p>
             <img src={ image } alt="Image Description" />
