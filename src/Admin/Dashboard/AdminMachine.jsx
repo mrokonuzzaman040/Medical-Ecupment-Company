3cd6c5ee3cb8cd153ab27503108da74f3721d9eb
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-26 04:35:06 +0600

    Add image upload functionality and implement machine addition

--- src/Admin/Dashboard/AdminMachine.jsx
+++ src/Admin/Dashboard/AdminMachine.jsx
@@ -1,12 +1,68 @@
 import React from 'react';
 import Header from '../../webcomponents/Carts/Header';
+import usePublicApi from '../AxiosHandeler/usePublicApi';
 
 const AdminMachine = () => {
+    const publicApi = usePublicApi();
+    const [ imageUrl, setImageUrl ] = React.useState( '' );
+
+    const uploadImage = async ( file ) => {
+        const formData = new FormData();
+        formData.append( 'image', file );
+
+        try {
+            const response = await fetch( '/api/uploadImage', {
+                method: 'POST',
+                body: formData,
+            } );
+
+            if ( response.ok ) {
+                const imageUrl = await response.text();
+                return imageUrl;
+            } else {
+                throw new Error( 'Image upload failed' );
+            }
+        } catch ( error ) {
+            console.error( error );
+            return null;
+        }
+    };
+
+    const handleImageUpload = async ( e ) => {
+        e.preventDefault();
+        const file = e.target.files[ 0 ];
+        const imageUrl = await uploadImage( file );
+        if ( imageUrl ) {
+            e.target[ 5 ].value = imageUrl;
+            setImageUrl( imageUrl );
+        }
+
+    };
+
+    const addMachine = ( e ) => {
+        e.preventDefault();
+        const name = e.target[ 0 ].value;
+        const details = e.target[ 1 ].value;
+        const brand = e.target[ 2 ].value;
+        const model = e.target[ 3 ].value;
+        const manufacture = e.target[ 4 ].value;
+        const image = e.target[ 5 ].value;
+        const machine = { name, details, brand, model, manufacture, image };
+
+        publicApi.post( '/addMachine', machine )
+            .then( ( response ) => {
+                console.log( response );
+            } )
+            .catch( ( error ) => {
+                console.error( error );
+            } );
+    };
+
     return (
         <div>
             <Header titel={ 'Machine' } />
             <div className="">
-                <form >
+                <form onSubmit={ addMachine } >
                     <div className="form-group">
                         <label htmlFor="name" className="block">Name</label>
                         <input type="text" className="form-control" id="name" placeholder="Enter Name" />
@@ -31,7 +87,7 @@ const AdminMachine = () => {
                         <label htmlFor="image" className="block">Image</label>
                         <input type="file" className="form-control" id="image" placeholder="Enter Image" />
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button onChange={ handleImageUpload } type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
         </div>
