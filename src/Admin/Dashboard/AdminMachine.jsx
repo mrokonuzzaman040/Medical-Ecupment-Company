7e1f6cab0cda98eb5f115ab1cd71ac7e654c8fed
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-26 21:53:46 +0600

    Update API base URL and refactor image upload logic

--- src/Admin/Dashboard/AdminMachine.jsx
+++ src/Admin/Dashboard/AdminMachine.jsx
@@ -4,40 +4,9 @@ import usePublicApi from '../AxiosHandeler/usePublicApi';
 
 const AdminMachine = () => {
     const publicApi = usePublicApi();
+    const [ file, setFile ] = React.useState( null );
     const [ imageUrl, setImageUrl ] = React.useState( '' );
 
-    const uploadImage = async ( file ) => {
-        const formData = new FormData();
-        formData.append( 'image', file );
-
-        try {
-            const response = await fetch( '/api/uploadImage', {
-                method: 'POST',
-                body: formData,
-            } );
-
-            if ( response.ok ) {
-                const imageUrl = await response.text();
-                return imageUrl;
-            } else {
-                throw new Error( 'Image upload failed' );
-            }
-        } catch ( error ) {
-            console.error( error );
-            return null;
-        }
-    };
-
-    const handleImageUpload = async ( e ) => {
-        e.preventDefault();
-        const file = e.target.files[ 0 ];
-        const imageUrl = await uploadImage( file );
-        if ( imageUrl ) {
-            e.target[ 5 ].value = imageUrl;
-            setImageUrl( imageUrl );
-        }
-
-    };
 
     const addMachine = ( e ) => {
         e.preventDefault();
@@ -47,7 +16,10 @@ const AdminMachine = () => {
         const model = e.target[ 3 ].value;
         const manufacture = e.target[ 4 ].value;
         const image = e.target[ 5 ].value;
-        const machine = { name, details, brand, model, manufacture, image };
+
+        const formData = new FormData();
+        formData.append( 'file', file );
+        const machine = { name, details, brand, model, manufacture, file };
 
         publicApi.post( '/addMachine', machine )
             .then( ( response ) => {
@@ -85,9 +57,9 @@ const AdminMachine = () => {
                     </div>
                     <div className="form-group">
                         <label htmlFor="image" className="block">Image</label>
-                        <input type="file" className="form-control" id="image" placeholder="Enter Image" />
+                        <input type="file" onChange={ ( e ) => { setFile( e.target.files[ 0 ] ) } } className="form-control" id="image" placeholder="Enter Image" />
                     </div>
-                    <button onChange={ handleImageUpload } type="submit" className="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
         </div>
