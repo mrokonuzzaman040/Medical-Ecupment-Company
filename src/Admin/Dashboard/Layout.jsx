c2edd4ece9b6728896296736cd03ba1ce641099f
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-02-29 01:22:59 +0600

    Add meta tags and update logo link in index.html, and add logout button in Layout.jsx

--- src/Admin/Dashboard/Layout.jsx
+++ src/Admin/Dashboard/Layout.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 import logo from './logo.png';
+import useAuth from '../../Hooks/useAuth';
 
 const Layout = () => {
+
+    const { logOut } = useAuth();
+
+    const handleLogOut = () => {
+        logOut()
+            .then( () => { } )
+            .catch( error => console.log( error ) );
+    }
+
     return (
         <div className="">
             <div className="flex">
                 <div className="w-64 bg-gray-800 h-screen">
                     <div className="flex items-center justify-center h-16 bg-gray-900 text-white">
-                        <img src={ logo } alt="logo" />
+                        <Link to={ '/' }><img src={ logo } alt="logo" /></Link>
                     </div>
                     <div className="flex flex-col items-left p-6 gap-5 h-16 bg-gray-800 text-white">
-                        <NavLink to={ '/dashboard' } className="hover:bg-gray-700 p-2 rounded">Dashboard</NavLink>
-                        <NavLink to={ '/dashboard/biochemistryreagents' } className="hover:bg-gray-700 p-2 rounded">Biochemistry Reagents</NavLink>
-                        <NavLink to={ '/dashboard/serologyreagents' } className="hover:bg-gray-700 p-2 rounded">Serology Reagents</NavLink>
-                        <NavLink to={ '/dashboard/elisareagents' } className="hover:bg-gray-700 p-2 rounded">ELISA Reagents</NavLink>
-                        <NavLink to={ '/dashboard/deviceandstrips' } className="hover:bg-gray-700 p-2 rounded">Device & Strips</NavLink>
-                        <NavLink to={ '/dashboard/consumableandaccessories' } className="hover:bg-gray-700 p-2 rounded">Consumable & Accessories</NavLink>
-                        <NavLink to={ '/dashboard/machine' } className="hover:bg-gray-700 p-2 rounded">Machine</NavLink>
+                        <NavLink to={ '/dashboard/biochemistryreagents' } className={ ( { isActive, isPending } ) => isActive ? 'border-b-4 border-green-500' : 'hover:border-b-4 hover:border-green-500' }>Biochemistry Reagents</NavLink>
+                        <NavLink to={ '/dashboard/serologyreagents' } className={ ( { isActive, isPending } ) => isActive ? 'border-b-4 border-green-500' : 'hover:border-b-4 hover:border-green-500' }>Serology Reagents</NavLink>
+                        <NavLink to={ '/dashboard/elisareagents' } className={ ( { isActive, isPending } ) => isActive ? 'border-b-4 border-green-500' : 'hover:border-b-4 hover:border-green-500' }>ELISA Reagents</NavLink>
+                        <NavLink to={ '/dashboard/deviceandstrips' } className={ ( { isActive, isPending } ) => isActive ? 'border-b-4 border-green-500' : 'hover:border-b-4 hover:border-green-500' }>Device & Strips</NavLink>
+                        <NavLink to={ '/dashboard/consumableandaccessories' } className={ ( { isActive, isPending } ) => isActive ? 'border-b-4 border-green-500' : 'hover:border-b-4 hover:border-green-500' }>Consumable & Accessories</NavLink>
+                        <NavLink to={ '/dashboard/machine' } className={ ( { isActive, isPending } ) => isActive ? 'border-b-4 border-green-500' : 'hover:border-b-4 hover:border-green-500' }>Machine</NavLink>
                     </div>
                 </div>
                 <div className="flex-1">
-                    <div className="flex items-center justify-center h-16 bg-gray-900 text-white">
+                    <div className="flex items-center justify-between p-4 h-16 bg-gray-900 text-white">
                         <h1 className="text-2xl">Khan Bio Tech <span>Dashboard</span></h1>
+                        <button className='btn btn-success' onClick={ handleLogOut }>Logout</button>
                     </div>
                     <div className="p-4 items-center justify-end flex flex-col">
                         <Outlet />
