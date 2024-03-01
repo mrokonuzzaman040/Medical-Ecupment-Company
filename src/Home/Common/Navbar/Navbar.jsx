c9f8a00c7becfecc9db65ccc24cacafa62bc01be
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-03-01 15:54:32 +0600

    Update Navbar styling

--- src/Home/Common/Navbar/Navbar.jsx
+++ src/Home/Common/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                     <div tabIndex={ 0 } role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={ 0 } className="menu bg-gray-400 text-white menu-sm dropdown-content mt-3 p-2 sm:z-[10] md:z-[10] lg:z-[0] shadow  rounded-box w-72">
+                    <ul tabIndex={ 0 } className="menu bg-gray-400 text-white menu-sm dropdown-content mt-3 p-2 z-[10] md:z-[10] lg:z-[0] shadow  rounded-box w-72">
                         <li>
                             <NavLink className={ ( { isActive, isPending } ) => isActive ? 'border-b-4 text-green-700 font-bold border-green-500' : 'hover:border-b-4 hover:border-green-500' } to={ '/' }>
                                 Home
