355090ed092e69d3df0057947227e83261c92cf5
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2024-03-02 19:22:16 +0600

    Ready For Prod

--- database.sql
+++ database.sql
@@ -1,21 +1,194 @@
---  Create table for this data
--- const data = {
---             name,
---             model,
---             brand,
---             manufacturer,
---             assembly,
---             image: imgUrl,
---             pdf: pdfUrl
---         }
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 02, 2024 at 07:21 PM
+-- Server version: 10.6.16-MariaDB-cll-lve
+-- PHP Version: 8.1.27
 
-CREATE TABLE machine (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    model VARCHAR(255),
-    brand VARCHAR(255),
-    manufacturer VARCHAR(255),
-    assembly VARCHAR(255),
-    image VARCHAR(255),
-    pdf VARCHAR(255)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `khanbiot_website`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bioreagent`
+--
+
+CREATE TABLE `bioreagent` (
+  `id` int(11) NOT NULL,
+  `name` text DEFAULT NULL,
+  `packSize` text DEFAULT NULL,
+  `test` text DEFAULT NULL,
+  `brand` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coadevice`
+--
+
+CREATE TABLE `coadevice` (
+  `id` int(11) NOT NULL,
+  `name` text DEFAULT NULL,
+  `packSize` text DEFAULT NULL,
+  `test` text DEFAULT NULL,
+  `brand` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `device`
+--
+
+CREATE TABLE `device` (
+  `id` int(11) NOT NULL,
+  `name` text DEFAULT NULL,
+  `packSize` text DEFAULT NULL,
+  `test` text DEFAULT NULL,
+  `brand` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `elsreagnt`
+--
+
+CREATE TABLE `elsreagnt` (
+  `id` int(11) NOT NULL,
+  `name` text DEFAULT NULL,
+  `packSize` text DEFAULT NULL,
+  `test` text DEFAULT NULL,
+  `brand` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `machine`
+--
+
+CREATE TABLE `machine` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `manufacturer` varchar(255) DEFAULT NULL,
+  `assembly` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `pdf` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `serreagent`
+--
+
+CREATE TABLE `serreagent` (
+  `id` int(11) NOT NULL,
+  `name` text DEFAULT NULL,
+  `packSize` text DEFAULT NULL,
+  `test` text DEFAULT NULL,
+  `brand` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bioreagent`
+--
+ALTER TABLE `bioreagent`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `coadevice`
+--
+ALTER TABLE `coadevice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `device`
+--
+ALTER TABLE `device`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `elsreagnt`
+--
+ALTER TABLE `elsreagnt`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `machine`
+--
+ALTER TABLE `machine`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `serreagent`
+--
+ALTER TABLE `serreagent`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bioreagent`
+--
+ALTER TABLE `bioreagent`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `coadevice`
+--
+ALTER TABLE `coadevice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `device`
+--
+ALTER TABLE `device`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `elsreagnt`
+--
+ALTER TABLE `elsreagnt`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `machine`
+--
+ALTER TABLE `machine`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `serreagent`
+--
+ALTER TABLE `serreagent`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
