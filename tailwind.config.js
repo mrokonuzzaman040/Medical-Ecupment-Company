041bec0c669ca77d2ed2e34675f98106cf728de2
rokondevolvedai <rokon@devolvedai.com>
2025-02-25 08:18:35 -0600

    Refactor Tailwind configuration to import daisyui directly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -41,6 +43,6 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [ require( "daisyui" ) ],
+  plugins: [ daisyui ],
 }
 
